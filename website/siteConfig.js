60e1baf7be6c3f2c23f787034ec8aec58be5fb78
Paul Reynolds <paul.reynolds@hotmail.co.uk>
2020-05-11 10:34:47 +0100

    config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Raiser WP', // Title for your website.
   tagline: 'Raiser WP Docs',
-  url: 'https://RaiserWeb.github.io', // Your website URL
+  url: 'https://raiserweb.github.io', // Your website URL
   baseUrl: '/Raiser-Wp-Docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
